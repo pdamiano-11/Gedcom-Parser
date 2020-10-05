3e9256acdc55b75378b0edb201b67461caf4c8b4
Michael McCreesh <43581672+mmccree1@users.noreply.github.com>
2020-10-04 20:37:27 -0400

    Add files via upload

---
+++ src/UserStories/US01.py
@@ -0,0 +1,37 @@
+'''
+Created on 09/26/20
+@author:   Michael McCreesh
+Pledge:    I pledge my honor to abide by the Stevens Honor System
+SSW555 - Sprint 1
+'''
+import datetime
+import pandas as pd
+import project02
+
+''' Goal: Ensure all dates are before current date'''
+def usecase01(gedcom_name):
+    today = datetime.datetime.today()
+    df = project02.createIndividualsDataFrame(gedcom_name)
+    df2 = project02.createFamiliesDataFrame(gedcom_name)
+    dateDict = []
+
+    for index, row in df.iterrows(): #iterates through dataframe
+        day = row['Birthday']
+        dateDict.append(day)
+    for index, row in df.iterrows(): #iterates through dataframe
+        day = row['Dead']
+        dateDict.append(day)
+    for index, row in df2.iterrows(): #iterates through dataframe
+        dateDict.append(row['Married'])
+    for index, row in df2.iterrows(): #iterates through dataframe
+       dateDict.append(row['Divorced'])
+    for date in dateDict:
+        if date == 'nan':
+            continue
+        else:
+            day = pd.to_datetime(date)
+            if day > today: return "There is at least one date later than the current date"
+    return "All dates are before today"
+if __name__ == '__main__':
+    file_name = input("Enter the name of the GEDCOM file: ")
+    usecase01(file_name)
