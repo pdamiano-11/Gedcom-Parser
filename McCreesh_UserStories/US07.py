dd73a6525804bd81c8a6350422eb7c5e322314e4
Michael McCreesh <43581672+mmccree1@users.noreply.github.com>
2020-10-04 20:22:04 -0400

    Add files via upload

--- McCreesh_UserStories/US07.py
+++ McCreesh_UserStories/US07.py
@@ -12,9 +12,7 @@ from dateutil.relativedelta import relativedelta
 ''' Goal: Ensure that no individuals live more than 150 years'''
 def usecase07(gedcom_name):
     today = datetime.datetime.today()
-
     df = project02.createIndividualsDataFrame(gedcom_name)
-
     for index, row in df.iterrows(): #iterates through dataframe
         birth = pd.to_datetime(row['Birthday'])
         if row['Dead'] == 'nan':
@@ -27,7 +25,6 @@ def usecase07(gedcom_name):
         if dif >= 150:
             return "At least one individual lived to be over 150"
     return "All ages are less than 150"
-
 if __name__ == '__main__':
     file_name = input("Enter the name of the GEDCOM file: ")
     usecase07(file_name)
