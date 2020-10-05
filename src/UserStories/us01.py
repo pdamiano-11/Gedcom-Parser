73264c0bc5f67755c342ae9b8030bc99339da635
pdamiano-11 <pdamiano@stevens.edu>
2020-10-05 00:01:55 -0400

    stuff

--- src/UserStories/us01.py
+++ src/UserStories/us01.py
@@ -1,24 +1,37 @@
 '''
-@author: Michael McCreesh
-
-User Story #01: Less then 150 years old
-
-Death should be less than 150 years after birth for 
-dead people, and current date should be less than 
-150 years after birth for all living people
+Created on 09/26/20
+@author:   Michael McCreesh
+Pledge:    I pledge my honor to abide by the Stevens Honor System
+SSW555 - Sprint 1
 '''
-
-import sys
-import os
 import datetime
+import pandas as pd
+import project02
 
-def us01(date):
-    today = datetime.datetime.now()
-    if date.year > today.year:
-        return 'This date is after current date.'
-    elif date.year == today.year and date.month > today.month:
-        return 'This date is after current date.'
-    elif date.year == today.year and date.month == today.month and date.day > today.day:
-        return 'This date is after current date.'
-    return 'This date is valid'
+''' Goal: Ensure all dates are before current date'''
+def usecase01(gedcom_name):
+    today = datetime.datetime.today()
+    df = project02.createIndividualsDataFrame(gedcom_name)
+    df2 = project02.createFamiliesDataFrame(gedcom_name)
+    dateDict = []
 
+    for index, row in df.iterrows(): #iterates through dataframe
+        day = row['Birthday']
+        dateDict.append(day)
+    for index, row in df.iterrows(): #iterates through dataframe
+        day = row['Dead']
+        dateDict.append(day)
+    for index, row in df2.iterrows(): #iterates through dataframe
+        dateDict.append(row['Married'])
+    for index, row in df2.iterrows(): #iterates through dataframe
+       dateDict.append(row['Divorced'])
+    for date in dateDict:
+        if date == 'nan':
+            continue
+        else:
+            day = pd.to_datetime(date)
+            if day > today: return "There is at least one date later than the current date"
+    return "All dates are before today"
+if __name__ == '__main__':
+    file_name = input("Enter the name of the GEDCOM file: ")
+    usecase01(file_name)
