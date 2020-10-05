4b8966b864f1e2020714dcdedbaaf97bb7d4952d
Michael McCreesh <43581672+mmccree1@users.noreply.github.com>
2020-10-04 20:40:37 -0400

    Delete us07_test.py

--- tests/us07_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import sys
-import os
-sys.path.append(os.path.abspath('../src/userstories'))
-from us07 import us07
-
-class us07_test(unittest.TestCase):
-    def test1(self):
-      return
