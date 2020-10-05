cf17da1c70e4102fdd2c3c2025c8cf4e97a9521b
pdamiano-11 <pdamiano@stevens.edu>
2020-10-05 14:24:49 -0400

    Update Project02.py

--- src/Project02.py
+++ src/Project02.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -149,54 +150,3 @@ def createFamiliesDataFrame(gedcom_name):
             families.Children[idx] = chil_ids
 
     return families
-
-
-def displayTable(gedcom_name):
-    individuals = createIndividualsDataFrame(gedcom_name)
-    families = createFamiliesDataFrame(gedcom_name)
-
-    otp = open("Project03_Output.txt", "w")
-    otp.truncate(0)
-    otp.write("Individuals: \n")
-    otp.write(tabulate(individuals, headers='keys', tablefmt='psql'))
-    otp.write("\n")
-    otp.write("Families: \n")
-    otp.write(str(families))
-    otp.close()
-
-def displayOutput(gedcom_name):
-    supported = ['INDI', '0 NOTE', '0 HEAD', '0 TRLR', 'FAM', '1 NAME', '1 SEX', '1 BIRT', '1 DEAT', '1 FAMC', 
-                 '1 FAMS', '1 MARR', '1 HUSB', '1 WIFE', '1 CHIL', '1 DIV', '2 DATE']
-
-    output_lines = []
-    input_lines = collectInputFile(gedcom_name)
-    for line in input_lines:
-        if any(s in line for s in supported):
-            t = line.strip().split()
-            if 'INDI' in t:
-                idx = t.index('INDI')
-                output_lines.append('|'.join([t[0]] + [t[idx]] + ['Y'] + [' '.join(t[1:idx])]))
-            elif 'FAM' in t:
-                idx = t.index('FAM')
-                output_lines.append('|'.join([t[0]] + [t[idx]] + ['Y'] + [' '.join(t[1:idx])]))
-            else:
-                output_lines.append('|'.join(t[0:2] + ['Y'] + [' '.join(t[2:])]))
-        else:
-            t = line.strip().split()
-            output_lines.append('|'.join(t[0:2] + ['N'] + [' '.join(t[2:])]))
-
-
-    otp = open("outputProject02.txt", "w")
-    for n in range(len(input_lines)):
-        otp.write("\n" + "--> " + input_lines[n])
-        otp.write("\n" + "<-- " + output_lines[n] + "\n")
-    otp.close()
-
-if __name__ == '__main__':
-    file_name = input("Enter the name of the GEDCOM file: ")
-    displayTable(file_name)
-
-
-
-
-
