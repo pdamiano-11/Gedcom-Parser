3328972ea629b31c9095829e7792af973ddcc96b
Michael McCreesh <43581672+mmccree1@users.noreply.github.com>
2020-10-05 18:02:36 -0400

    Update us01_test.py

--- tests/us01_test.py
+++ tests/us01_test.py
@@ -1,25 +1,25 @@
 import unittest
 import sys
 import os
-sys.path.append(os.path.abspath('../src/userstories'))
-import US01
+sys.path.append(os.path.abspath('..'))
+import us01
 
 # ----------------------------------- Test Cases for Use Case 41 -----------------------------------
 class usecase01_test(unittest.TestCase):
     def test1(self):
-        self.assertEqual(US01.usecase01("testFiles/test8.ged"),'All dates are before today')
+        self.assertEqual(us01.usecase01("seeds/test8.ged"),'All dates are before today')
 
     def test2(self):
-            self.assertEqual(US01.usecase01("testFiles/test9.ged"),'All dates are before today')
+            self.assertEqual(us01.usecase01("seeds/test9.ged"),'All dates are before today')
 
     def test3(self):
-            self.assertEqual(US01.usecase01("testFiles/test10.ged"),'All dates are before today')
+            self.assertEqual(us01.usecase01("seeds/test10.ged"),'All dates are before today')
 
     def test4(self):
-            self.assertEqual(US01.usecase01("testFiles/test11.ged"),"There is at least one date later than the current date")
+            self.assertEqual(us01.usecase01("seeds/test11.ged"),"There is at least one date later than the current date")
 
     def test5(self):
-            self.assertEqual(US01.usecase01("testFiles/test12.ged"),"There is at least one date later than the current date")
+            self.assertEqual(us01.usecase01("seeds/test12.ged"),"There is at least one date later than the current date")
 
 if __name__ == '__main__':
     unittest.main()
